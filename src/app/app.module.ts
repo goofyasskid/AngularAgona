f6278f9737d32962963c25b30cd2eb613eda068c
goofyasskid <sergio.shorin@mail.ru>
2022-09-22 12:52:51 +0300

    base architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MarketComponent } from './pages/market/market.component';
+import { MainComponent } from './pages/main/main.component';
+import { PointsComponent } from './pages/points/points.component';
+import { UserComponent } from './pages/user/user.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarketComponent,
+    MainComponent,
+    PointsComponent,
+    UserComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
